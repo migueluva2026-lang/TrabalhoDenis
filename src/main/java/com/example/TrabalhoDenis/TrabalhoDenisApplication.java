8d81aaf2ba56328a5484b5c0b51161a69966a1b5
ErmDev <migueluva2026@gmail.com>
2026-04-21 15:44:09 -0300

    Initial commit

---
+++ src/main/java/com/example/TrabalhoDenis/TrabalhoDenisApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrabalhoDenis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDenisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDenisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TrabalhoDenis/TrabalhoDenisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrabalhoDenis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoDenisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
